91360212c016b9b24c31dec68401e0b2ee2a960d
josebagar@gmail.com <josebagar@gmail.com@cf1cfcf2-4bed-3a2c-c295-7c4846a95009>
2010-06-21 02:03:33 +0000

    Not using SDL_Surface anymore. Instruct libvlc to write data to a GRAPH directly.

--- trunk/make.sh
+++ trunk/make.sh
@@ -9,16 +9,16 @@ if [ -f $MODULE.so ]; then
 fi
 
 # Compile the source code
-gcc -c -Wall $(pkg-config --cflags $DEPS) $MODULE.c
+gcc -c -Wall $(pkg-config --cflags $DEPS) $MODULE-1.1.c
 
 # Link the module
-if [ -f $MODULE.o ]; then
-	gcc -o$MODULE.so $MODULE.o -lpthread -shared $(pkg-config --libs $DEPS) -Wl,-soname -Wl,$MODULE.so
+if [ -f $MODULE-1.1.o ]; then
+	gcc -o$MODULE.so $MODULE-1.1.o -shared $(pkg-config --libs $DEPS) -Wl,-soname -Wl,$MODULE.so
 fi
 
 # Strip and remove compilation files
 if [ -f $MODULE.so ]; then
-	rm $MODULE.o
-	strip $MODULE.so
+	rm $MODULE-1.1.o
+#	strip $MODULE.so
 fi
 
